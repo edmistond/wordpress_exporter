24ad0029cbcf59d949c9c2ad33768fc599d86be6
David Edmiston <edmistond@gmail.com>
2013-12-06 23:09:27 -0500

    Refactor post-building code
    Radical simplification to remove comments stuff and parse it out into a module
    instead of using a bunch of classes. Just parses through the file now, parses
    the HTML, and writes out a bunch of markdown files.

--- export.rb
+++ export.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require './post.rb'
 
 # wordpress.xml is your exported wordpress XML file
-f = File.open('wordpress.xml')
+f   = File.open('wordpress.xml')
 doc = Nokogiri::XML(f)
 f.close
 
@@ -12,34 +12,7 @@ doc.remove_namespaces!
 
 items = doc.css('item')
 
-posts = Array.new()
-
 items.each do |i|
-	post = Post.new(i.css('post_id').text, i.css('title').text)
-	post.tags = i.css('category[nicename]').map(&:text)
-	post.date = DateTime.parse(i.css('pubDate').text)
-	post.link = i.css('link').text
-	post.content = i.css('encoded').text
-
-    comments = i.css('comment')
-    if (comments.count>0)
-        post.comments = Array.new()
-        comments.each do |c|
-            comment = Comment.new()
-            comment.author_name = c.css('comment_author').text
-            comment.date = c.css('comment_date').text
-            comment.content = c.css('comment_content').text
-            post.comments << comment
-        end
-    end
-
-	posts << post
+  post_tags = i.css('category[nicename]').map(&:text)
+  WordpressParser.parse_post(i, post_tags) unless post_tags.include?('Twitters')
 end
-
-posts.reject { |p| p.tags.include?('Twitters') || p.comment_count == 0 }.each do |p|
-	puts "#{p.id} - #{p.title}"
-    converter = PandocRuby.new(p.content, :from => :html, :to  => :markdown)
-    p.markdown_content = converter.convert
-    puts p.markdown_content
-end
-
--- post.rb
+++ post.rb
@@ -1,17 +1,42 @@
-class Post
-	attr_accessor :id, :title, :tags, :date, :link, :content, :comments, :markdown_content
+require 'pandoc-ruby'
+require 'nokogiri'
+require 'date'
 
-	def initialize(id, title)
-		@id = id
-		@title = title
-	end
+module WordpressParser
 
-    def comment_count
-        return @comments.length unless comments == nil
-        return 0
+  POST_TEMPLATE = <<EOS
+--------
+title: %TITLE%
+post_date: %POST_DATE%
+tags: %TAGS%
+--------
+
+EOS
+
+  def build_file_name(post_name, post_date)
+    return post_date.strftime("%Y-%m-%d-#{post_name}")
+  end
+
+  def parse_post(post_xml, post_tags)
+    post_front_matter = String.new(POST_TEMPLATE)
+
+    post_front_matter['%TITLE%']    = post_xml.css('title').text
+    post_date                       = DateTime.parse(post_xml.css('pubDate').text)
+    post_front_matter['%POST_DATE%'] = post_date.strftime('%Y-%m-%d')
+    post_front_matter['%TAGS%']     = post_tags.join(',') #(post_xml.css('category[nicename]').map(&:text)).join(',')
+
+    file_name = build_file_name(post_xml.css('post_name').text, post_date)
+
+    post_html     = post_xml.css('encoded').text
+    converter     = PandocRuby.new(post_html, :from => :html, :to => :markdown)
+    post_markdown = converter.convert
+
+    puts "writing #{file_name}"
+    File.open("output/#{file_name}.md", 'w') do |f|
+      f.puts(post_front_matter)
+      f.puts(post_markdown)
     end
+  end
+  module_function :parse_post, :build_file_name
 end
 
-class Comment
-    attr_accessor :author_name, :content, :date, :markdown_content
-end
