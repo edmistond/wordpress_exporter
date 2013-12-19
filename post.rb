2e117bd7169c955829f6713c0db8dda8eef9d187
David Edmiston <edmistond@gmail.com>
2013-12-19 18:50:47 -0500

    Updated rakefile, gitignore, and post

--- post.rb
+++ post.rb
@@ -6,9 +6,11 @@ module WordpressParser
 
   POST_TEMPLATE = <<EOS
 ---
-title: %TITLE%
+title: "%TITLE%"
 post_date: %POST_DATE%
-tags: %TAGS%
+slug: %SLUG%
+tags:
+%TAGS%
 ---
 
 EOS
@@ -20,11 +22,19 @@ EOS
   def parse_post(post_xml, post_tags)
     post_front_matter = String.new(POST_TEMPLATE)
 
-    post_front_matter['%TITLE%'] = post_xml.css('title').text
+    post_front_matter['%TITLE%'] = post_xml.css('title').text.gsub(/\"/, '\"')
     post_date = DateTime.parse(post_xml.css('pubDate').text)
     post_front_matter['%POST_DATE%'] = post_date.strftime('%Y-%m-%d')
-    post_front_matter['%TAGS%'] = post_tags.join(',')
-    file_name = build_file_name(post_xml.css('post_name').text, post_date)
+
+    tag_text = String.new()
+    post_tags.each do |t|
+      tag_text << "- #{t}\n"
+    end
+    post_front_matter['%TAGS%'] = tag_text
+
+    post_name = post_xml.css('post_name').text
+    post_front_matter['%SLUG%'] = post_name
+    file_name = build_file_name(post_name, post_date)
 
     post_html = post_xml.css('encoded').text
 
@@ -37,11 +47,14 @@ EOS
     # clean up a few annoying things pandoc is doing
     post_markdown.gsub!(/\\\n/, "\n") # fix linebreaks that are getting escaped too
     post_markdown.gsub!(/\\\$/, '$') # stop trying to escape dollar signs
+    post_markdown.gsub!(/\\#/, '#') # fix \# back to #
 
     puts "writing #{file_name}"
     File.open("output/#{file_name}.md", 'w') do |f|
       f.puts(post_front_matter)
+      # f.puts('{{#markdown}}')
       f.puts(post_markdown)
+      # f.puts('{{/markdown}}')
     end
   end
 
